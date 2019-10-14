1028edecfe106d9b2ae73eabd444ba82d50fdce3
Gonzalo <gonzacalleja@gmail.com>
2019-10-14 11:36:45 +0200

    First commit from fork

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello World, I'm Clara");
+        System.out.println("Hello World, I'm Clara\n Hi Clara I forked your project");
     }
 }
